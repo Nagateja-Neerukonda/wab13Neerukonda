ebbf649bd1a2f7604b9062813f57abdad655b765
Tejausa <tejausa123@gmail.com>
2021-03-31 20:31:41 -0500

    Bonus added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var foodRouter = require('./routes/food');
 var starRouter = require('./routes/star');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/food', foodRouter);
 app.use('/star', starRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
